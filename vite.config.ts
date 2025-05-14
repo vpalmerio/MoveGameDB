45a23311e84a8ef2318d398a082b007df51ce88f
vpalmerio <vpalmerio@protonmail.com>
2025-05-14 18:18:12 -0400

    Fix "Buffer not defined" issue with node polyfill

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,8 @@
 import path from "path";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import { nodePolyfills } from 'vite-plugin-node-polyfills';
+
 
 export default defineConfig({
   build: {
@@ -11,10 +13,18 @@ export default defineConfig({
   },
   plugins: [
     react(),
+    nodePolyfills({
+      globals: {
+        Buffer: true, // Enable Buffer polyfill
+      },
+    }),
   ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./frontend"),
     },
+  },
+  optimizeDeps: {
+    include: ['buffer'],
   },
 });
